40d7c29538247d84951250196d876d441761c28a
KenHV <kenhv@null.net>
2020-05-08 12:57:09 +0530

    vince: bringup havoc
    Signed-off-by: KenHV <kenhv@null.net>

--- havoc_vince.mk
+++ havoc_vince.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2017 The aospOS Project
+# Copyright (C) 2017 The havocOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,16 +17,18 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/havoc/config/phone-xxhdpi-4096-dalvik-heap.mk)
+$(call inherit-product, vendor/havoc/config/phone-xxhdpi-2048-hwui-memory.mk)
 
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common AOSiP stuff.
-$(call inherit-product, vendor/aosip/config/common_full_phone.mk)
+# Inherit some common havocOS stuff.
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := aosip_vince
+PRODUCT_NAME := havoc_vince
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
--- prop.mk
+++ prop.mk
@@ -15,3 +15,6 @@ vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
 
+# Havoc maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.havoc.maintainer=KenHV
